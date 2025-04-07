f39fac82eb2bf8c505e8814e91184f2bdd8a9d76
danyilispaniuk <danulispanyk@gmail.com>
2025-04-07 17:43:01 +0200

    Docker container

--- src/AngularProject/src/main.ts
+++ src/AngularProject/src/main.ts
@@ -4,7 +4,7 @@ import { AppComponent } from './app/app.component';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideToastr } from 'ngx-toastr';
 // bootstrap import
-import "bootstrap-icons/font/bootstrap-icons.css";
+// import "bootstrap-icons/font/bootstrap-icons.css";
 
 
 bootstrapApplication(AppComponent, { 
