7ef0481953972129b206827ac6219127cfa30289
filip <filip.chromecka@outlook.com>
2025-04-13 19:18:54 +0200

    Return ApiResult as response

--- src/BookingService.API/Features/Bookings/BookingsController.cs
+++ src/BookingService.API/Features/Bookings/BookingsController.cs
@@ -4,6 +4,7 @@ using BookingService.API.Features.Bookings.Models;
 using BookingService.API.Features.Bookings.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using ReservationSystem.Shared.Results;
 using System.Net;
 
 namespace BookingService.API.Features.Bookings;
@@ -13,26 +14,26 @@ public sealed class BookingsController(IMediator mediator) : ApiControllerBase(m
 {
     [HttpGet]
     //[Authorize]
-    [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(IList<BookingResponse>))]
-    //[ProducesResponseType((int)HttpStatusCode.Unauthorized, Type = typeof(ProblemDetails))]
+    [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(ApiResult<IList<BookingResponse>>))]
+    //[ProducesResponseType((int)HttpStatusCode.Unauthorized, Type = typeof(ApiResult<ProblemDetails>))]
     public async Task<IActionResult> Get(BookingStatus? status)
         => await ExecuteAsync(new GetBookingsQuery(status));
 
     [HttpGet("{bookingId}")]
-    [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(BookingResponse))]
-    [ProducesResponseType((int)HttpStatusCode.NotFound, Type = typeof(ProblemDetails))]
+    [ProducesResponseType((int)HttpStatusCode.OK, Type = typeof(ApiResult<BookingResponse>))]
+    [ProducesResponseType((int)HttpStatusCode.NotFound, Type = typeof(ApiResult<ProblemDetails>))]
     public async Task<IActionResult> GetDetail(int bookingId)
         => await ExecuteAsync(new GetBookingDetailQuery(bookingId));
 
     [HttpPost]
     [ProducesResponseType((int)HttpStatusCode.Created)]
-    [ProducesResponseType((int)HttpStatusCode.BadRequest, Type = typeof(ValidationProblemDetails))]
+    [ProducesResponseType((int)HttpStatusCode.BadRequest, Type = typeof(ApiResult<ValidationProblemDetails>))]
     public async Task<IActionResult> Create(CreateBookingRequest booking)
        => await ExecuteWithCreatedAtActionAsync(new CreateBookingCommand(booking), nameof(GetDetail), response => new { bookingId = response.Id });
 
     [HttpDelete("{bookingId}")]
     [ProducesResponseType((int)HttpStatusCode.NoContent)]
-    [ProducesResponseType((int)HttpStatusCode.NotFound, Type = typeof(ProblemDetails))]
+    [ProducesResponseType((int)HttpStatusCode.NotFound, Type = typeof(ApiResult<ProblemDetails>))]
     public async Task<IActionResult> Delete(int bookingId)
         => await ExecuteWithNoContentAsync(new DeleteBookingCommand(bookingId));
 }
