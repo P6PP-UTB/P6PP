cab92561d522d805275bbe6f510f947d744705f2
danyilispaniuk <danulispanyk@gmail.com>
2025-04-14 19:48:34 +0200

    Small changes

--- src/AngularProject/src/app/pages/profile/profile.page.ts
+++ src/AngularProject/src/app/pages/profile/profile.page.ts
@@ -91,7 +91,7 @@ export class ProfilePage implements OnInit {
   }
 
   onDeleteAccount() {
-    const confirmDelete = window.confirm('Are you sure you want to delete your account? This action is irreversible.');
+    const confirmDelete = window.confirm('Are you sure you want to send a request to delete your account? This action is irreversible.'); // Are you sure you want to delete your account? This action is irreversible.
     if (confirmDelete) {
       this.userService.deleteUser(this.user.id).subscribe(() => {
         localStorage.removeItem('token');
