62f293333425c9e5ad00e1e4b1c4885f6d2c30b2
danyilispaniuk <danulispanyk@gmail.com>
2025-04-22 11:49:36 +0200

    exported interfaces

--- src/AngularProject/src/app/services/auth.service.ts
+++ src/AngularProject/src/app/services/auth.service.ts
@@ -1,19 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
-
-interface RegisterPayload {
-  userName: string;
-  email: string;
-  password: string;
-  firstName: string;
-  lastName: string;
-}
-
-interface LoginPayload {
-  usernameOrEmail: string;
-  password: string;
-}
+import { RegisterPayload } from './interfaces/registerPayload';
+import { LoginPayload } from './interfaces/loginPayload';
 
 @Injectable({ providedIn: 'root' })
 export class AuthService {
