1426ccf989a30f7c9ad8f2e2cf422328caab3813
danyilispaniuk <danulispanyk@gmail.com>
2025-04-25 01:10:17 +0200

    course styling (by Mariia V)

--- src/AngularProject/src/app/pages/course-page/course-page.component.ts
+++ src/AngularProject/src/app/pages/course-page/course-page.component.ts
@@ -6,73 +6,73 @@ import { CourseService } from '../../services/course.service';
 import { UserService } from '../../services/user.service';
 import { Course } from '../../services/interfaces/course';
 
-
 @Component({
   selector: 'app-course-page',
+  standalone: true,
   imports: [CommonModule, NavigationComponent, FooterComponent],
   templateUrl: './course-page.component.html',
-  styleUrl: './course-page.component.scss'
+  styleUrls: ['./course-page.component.scss']
 })
 export class CoursePageComponent {
-
-  constructor(
-    private courseService: CourseService,
-    private userService: UserService
-  ){
-
-  }
-
-  // course: Course = {
-  //     id: 1,
-  //     trainerId: 1,
-  //     start: new Date(),
-  //     end: new Date(),
-  //     price: 100,
-  //     serviceName: "Swimming",
-  //     currentCapacity: 35,
-  //     totalCapacity: 40,
-  //     roomName: "Swimming Pool 1",
-  //     isCancelled: false
-  //   }
-
-  // course: any | null = null;
   course: Course = {
     id: 99999999,
     trainerId: 99999999,
     start: new Date(),
     end: new Date(),
     price: 99999999,
-    serviceName: "Testing",
+    serviceName: "ServiceName",
     currentCapacity: 99999999,
     totalCapacity: 4999999990,
-    roomName: "Testing",
+    roomName: "RoomName",
     isCancelled: false
   };
 
-  trainer: any;
+  trainer: string = '';
+
+  // Массив изображений
+  imageUrls: string[] = [
+    'https://goo.su/kSY4URo?',
+    'https://goo.su/NHmvY',
+    'https://goo.su/YyOz7YE',
+    'https://tse3.mm.bing.net/th?id=OIG2.5bowY.5mQdQKTIv5qY1g&pid=ImgGn'
+  ];
+  currentIndex = 0;
+
+  // Геттер для текущего изображения
+  get currentImage(): string {
+    return this.imageUrls[this.currentIndex];
+  }
+
+  // Метод для перехода к предыдущему изображению
+  prevImage(): void {
+    this.currentIndex = (this.currentIndex - 1 + this.imageUrls.length) % this.imageUrls.length;
+  }
+
+  // Метод для перехода к следующему изображению
+  nextImage(): void {
+    this.currentIndex = (this.currentIndex + 1) % this.imageUrls.length;
+  }
+
+  constructor(
+    private courseService: CourseService,
+    private userService: UserService
+  ) {}
 
   async ngOnInit() {
     const currentUrl: string = window.location.href;
     const id = this.getLastSegment(currentUrl);
-  
-    await this.courseService.getOneCourse(id).subscribe(response => {
-      console.log('Fetched response:', response);
+
+    this.courseService.getOneCourse(id).subscribe(response => {
       this.course = response.data;
-      console.log('Fetched course:', this.course);
-      this.userService.getUserById(this.course.trainerId).subscribe( trainerResponse => {
-        console.log("Trainer: ", trainerResponse);
-        this.trainer = trainerResponse.data.user.firstName + " " + trainerResponse.data.user.lastName;
+      this.userService.getUserById(this.course.trainerId).subscribe(trainerResponse => {
+        this.trainer = trainerResponse.data.user.firstName + ' ' + trainerResponse.data.user.lastName;
       });
     });
-
-
   }
-  
 
+  // Функция для получения последнего сегмента URL
   private getLastSegment(url: string): string {
-    const parts = url.split("/");
-    const result = parts.pop();
-    return result || "1";
+    const parts = url.split('/');
+    return parts.pop() || '1';
   }
-  
 }
