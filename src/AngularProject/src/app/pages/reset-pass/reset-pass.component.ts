b3408b815b1448f09424debf7f4e56a361ee214f
danyilispaniuk <danulispanyk@gmail.com>
2025-04-24 10:45:51 +0200

    Reset password logic v1

--- src/AngularProject/src/app/pages/reset-pass/reset-pass.component.ts
+++ src/AngularProject/src/app/pages/reset-pass/reset-pass.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-reset-pass',
@@ -15,9 +17,11 @@ export class ResetPassComponent {
   
   constructor(
     private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private http: HttpClient
   ) {
     this.resetForm = this.fb.group({ 
-      password: ['', [
+      newPassword: ['', [
         Validators.required, 
         Validators.minLength(8),
         Validators.pattern('^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[^A-Za-z0-9]).{8,}$')]],
@@ -30,10 +34,27 @@ export class ResetPassComponent {
   onSubmit() {  
     if (this.resetForm.valid) {
       const form = this.resetForm.value;
+
+      this.route.queryParams.subscribe(params => {
+        const userId = params['userId'];
+        const token = params['token'];
   
-      const payload = {
-        password: form.password
-      };
+        const payload = {
+          newPassword: form.newPassword,
+          token: token,
+          userId: userId
+        };
+        console.log(payload)
+
+        this.http.post('http://localhost:8005/api/auth/change-password', payload)
+        .subscribe({
+          next: () => alert('Your password has been changed'),
+          error: (err) => {
+            console.error(err);
+            alert('Error: ' + (err.error?.message || 'unexpected error'));
+          }
+        });
+      });
     }
   }
 
