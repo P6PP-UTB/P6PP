aa7adc78899d847e54ab0d6c21dd9390dc03861a
Robin Tetour <git@robtet.dev>
2025-04-28 22:03:11 +0200

    rebasing simon/auth (#2)
    * Add Payments.API project
    
    * Add PaymentService.API project and related features
    
    * Add payment service and restructure application
    
    - Updated `docker-compose.yml` to include `payment-service`.
    - Added `PaymentService` class in `ServiceEndpoints.cs` for payment operations.
    - Modified logging in `DatabaseInitializer.cs` for new database tables.
    - Changed connection string in `appsettings.json` to use `mysql`.
    - Removed `WeatherForecastController.cs` and `WeatherForecast.cs`.
    - Cleared content in `Payments.API.csproj` and `Payments.API.sln`.
    - Updated host address in `Payments.API.http`.
    - Removed service registration and middleware in `Program.cs`.
    - Adjusted `launchSettings.json` for new application URLs.
    - Cleared logging configurations in `appsettings.Development.json` and `appsettings.json`.
    
    * Add missing path for new service
    
    * Refactor and used folder for context
    
    * simplifying
    
    * Applying workaround
    
    * Aktualizace konfigurace: .gitignore, DB, API endpoint
    
    * Refactor namespaces and enhance database seeding
    
    * Opravy namespace a aktualizace DatabaseSeeder
    
    * Fix endpoint route for GetBalanceById to use correct path
    
    * Update API endpoints for payment and balance services
    
    * Opravy URL, SQL dotazů a logiky vkládání dat
    
    * Add RoleId to Payment handling and database storage
    
    * Opravy endpointu a úpravy SQL logiky v PaymentRepo
    
    * Refactor UpdatePayment endpoint to remove ID parameter
    
    * Refactor payment ID handling in service classes
    
    * Opravy logiky a sjednocení formátů ve zpracování plateb
    
    * Room post fix
    
    ---------
    
    Co-authored-by: d2_hlavicka <d2_hlavicka@utb.cz>
    Co-authored-by: MartinBenicek <martabenic@seznam.cz>
    Co-authored-by: Martin Beníček <63211618+MartinBenicek@users.noreply.github.com>
    Co-authored-by: Šimon Bučka <118976633+sBucka@users.noreply.github.com>
    Co-authored-by: msichm <m_sichman@utb.cz>
    Co-authored-by: Jiří Drápal <41378892+JustJirka@users.noreply.github.com>

---
+++ src/PaymentService.API/Extensions/ServiceExtensions.cs
@@ -0,0 +1,52 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ReservationSystem.Shared.Clients;
+using PaymentService.API.Features;
+using PaymentService.API.Features.Payments;
+using PaymentService.API.Persistence.Repositories;
+using PaymentService.API.Services;
+
+using DatabaseInitializer = PaymentService.API.Persistence.DatabaseInitializer;
+using PaymentService.API.Persistence;
+
+namespace PaymentService.API.Extensions;
+
+public static class ServiceExtensions
+{
+    public static void RegisterServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        // Register Db Context 
+        services.AddSingleton<DapperContext>();
+        services.AddSingleton<DatabaseInitializer>();
+        services.AddSingleton<DatabaseSeeder>();
+        
+        // Register Repositories
+        services.AddScoped<PaymentRepository>();
+        
+        // Register Services
+        services.AddScoped<Services.PaymentService>();
+       
+        
+        // Register Endpoints injections
+        services.AddScoped<CreateBalanceHandler>();
+        services.AddSingleton<CreateBalanceValidator>();
+
+        services.AddScoped<UpdatePaymentHandler>();
+        services.AddSingleton<UpdatePaymentValidator>();
+
+        services.AddScoped<GetBalanceByIdHandler>();
+        services.AddSingleton<GetBalanceByIdValidator>();
+
+        services.AddScoped<GetPaymentByIdHandler>();
+        services.AddSingleton<GetPaymentByIdValidator>();
+
+        services.AddScoped<CreatePaymentHandler>();
+        services.AddSingleton<CreatePaymentValidator>();
+
+        // HttpClient
+        services.AddHttpClient<NetworkHttpClient>();
+        
+        // Register Memory Cache
+        services.AddMemoryCache();
+    }
+}
---
+++ src/PaymentService.API/Program.cs
@@ -0,0 +1,63 @@
+using PaymentService.API.Data;
+using PaymentService.API.Extensions;
+using PaymentService.API.Features;
+using PaymentService.API.Features.Payments;
+using PaymentService.API.Persistence;
+using PaymentService.API.Persistence;
+// Ensure the correct namespace is used for the DatabaseInitializer and DatabaseSeeder
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenAnyIP(5185);
+});
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<DapperContext>();
+
+
+builder.Services.RegisterServices(builder.Configuration);
+
+var Services = builder.Services;
+var configuration = builder.Configuration;
+
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var databaseInitializer = services.GetRequiredService<DatabaseInitializer>();
+    await databaseInitializer.InitializeDatabaseAsync();
+
+    //// Seed the database
+    var dbSeeder = services.GetRequiredService<DatabaseSeeder>();
+    await dbSeeder.SeedAsync();
+}
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    // USER ENDPOINTS
+    
+    CreatePaymentEndpoint.Register(endpoints);
+    GetPaymentByIdEndpoint.Register(endpoints);
+    UpdatePaymentEndpoint.Register(endpoints);
+    GetBalanceByIdEndpoint.Register(endpoints);
+    CreateBalanceEndpoint.Register(endpoints);
+
+});
+
+app.Run();
