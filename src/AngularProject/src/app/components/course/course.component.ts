6e41f12cd9c8ad82e485eb74bc93ef737dbb5a2f
Nikita Yalynnyi <n_yalynnyi@utb.cz>
2025-04-30 13:47:27 +0200

    course info added

--- src/AngularProject/src/app/components/course/course.component.ts
+++ src/AngularProject/src/app/components/course/course.component.ts
@@ -1,32 +1,49 @@
-import { ToastrService } from 'ngx-toastr';
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Course } from '../../services/interfaces/course';
 import { CourseService } from '../../services/course.service';
+import { UserService } from '../../services/user.service';
+import { ToastrService } from 'ngx-toastr';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-course',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './course.component.html',
-  styleUrl: './course.component.scss'
+  styleUrls: ['./course.component.scss']
 })
-export class CourseComponent {
+export class CourseComponent implements OnInit {
   @Input() course!: Course;
+
+  trainerName: string = '';
 
   constructor(
     private courseService: CourseService,
+    private userService: UserService,
     private toastr: ToastrService
   ) {}
 
+  ngOnInit(): void {
+    if (this.course?.trainerId) {
+      this.userService.getUserById(this.course.trainerId).subscribe({
+        next: (res) => {
+          this.trainerName = `${res.firstName} ${res.lastName}`;
+        },
+        error: () => {
+          this.toastr.error('Failed to load trainer info', 'Error');
+        }
+      });
+    }
+  }
+
   reserveCourse(serviceId: number): void {
     this.courseService.bookService(serviceId).subscribe({
-      next: (response: any) => {
-        // console.log('Booking successful:', response);
+      next: () => {
         this.toastr.success('Course reserved successfully!', 'Success');
       },
       error: (err: any) => {
-        // console.error('Error booking course:', err);
         const errorMessage = err.error?.message || 'An error occurred while booking the course.';
         this.toastr.error(errorMessage, 'Error');
       }
