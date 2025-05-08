a6860287e1ba4408cbe53232a122456cf38c10ac
Robin Tetour <git@robtet.dev>
2025-05-09 00:17:58 +0200

    Env file

---
+++ src/AngularProject/src/enviroments/environment.dev-deployment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    api: {
+      auth: 'https://authservice.thankfulflower-27b66160.polandcentral.azurecontainerapps.io/api/auth',
+      user: 'https://userservice.thankfulflower-27b66160.polandcentral.azurecontainerapps.io/api/user',
+      notification: 'https://notificationservice.thankfulflower-27b66160.polandcentral.azurecontainerapps.io/api/notification',
+      payment: 'https://paymentservice.thankfulflower-27b66160.polandcentral.azurecontainerapps.io/api',
+      course: 'https://bookingservice.thankfulflower-27b66160.polandcentral.azurecontainerapps.io/api',
+      analytics: 'https://analyticsservice.thankfulflower-27b66160.polandcentral.azurecontainerapps.io/api'
+    }
+  };
+  
