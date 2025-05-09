1c05fbe42d7c37889a4cbd0f046f6b1beb127269
d2_hlavicka <d2_hlavicka@utb.cz>
2025-05-09 22:15:20 +0200

    Remove database seeding logic and related classes

--- src/PaymentService.API/Persistence/DatabaseSeeder.cs
+++ src/PaymentService.API/Persistence/DatabaseSeeder.cs
@@ -16,54 +16,9 @@ public class DatabaseSeeder
 
     public async Task SeedAsync()
     {
-        await using var connection = await _context.CreateConnectionAsync();
+       
 
-        _logger.LogInformation("Seeding Payments...");
-
-        const string insertPaymentsQuery = @"
-                   INSERT INTO Payment (PaymentID, UserId, RoleId, CreditAmount, Price, Status, TransactionType, CreatedAt)
-            SELECT * FROM (SELECT 1 AS PaymentID, 3 AS UserId, 1 AS RoleId, 250 AS CreditAmount, 0 AS Price, 'completed' AS Status, 'credit' AS TransactionType, NOW() AS CreatedAt) AS tmp
-            WHERE NOT EXISTS (
-                SELECT 1 FROM Payment WHERE PaymentID = 1
-            )
-            UNION ALL
-            SELECT * FROM (SELECT 2 AS PaymentID, 2 AS UserId, 3 AS RoleId, 0 AS CreditAmount, 200 AS Price, 'completed' AS Status, 'reservation' AS TransactionType, NOW() AS CreatedAt) AS tmp
-            WHERE NOT EXISTS (
-                SELECT 1 FROM Payment WHERE PaymentID = 2
-            )
-            UNION ALL
-            SELECT * FROM (SELECT 3 AS PaymentID, 3 AS UserId, 1 AS RoleId, 0 AS CreditAmount, 500 AS Price, 'failed' AS Status, 'reservation' AS TransactionType, NOW() AS CreatedAt) AS tmp
-            WHERE NOT EXISTS (
-                SELECT 1 FROM Payment WHERE PaymentID = 3
-            );
-        ";
-
-        await connection.ExecuteAsync(insertPaymentsQuery);
-
-        _logger.LogInformation("Payments seeded successfully.");
-
-        _logger.LogInformation("Seeding UserCredit...");
-
-        const string insertUserCreditQuery = @"
-               INSERT INTO UserCredit (UserId, CreditBalance)
-        SELECT * FROM (SELECT 1 AS UserId, 1000 AS CreditBalance) AS tmp
-        WHERE NOT EXISTS (
-            SELECT 1 FROM UserCredit WHERE UserId = 1
-        )
-        UNION ALL
-        SELECT * FROM (SELECT 2 AS UserId, 500 AS CreditBalance) AS tmp
-        WHERE NOT EXISTS (
-            SELECT 1 FROM UserCredit WHERE UserId = 2
-        )
-        UNION ALL
-        SELECT * FROM (SELECT 3 AS UserId, 250 AS CreditBalance) AS tmp
-        WHERE NOT EXISTS (
-            SELECT 1 FROM UserCredit WHERE UserId = 3
-        );
-        ";
-        await connection.ExecuteAsync(insertUserCreditQuery);
-        _logger.LogInformation("UserCredit seeded successfully.");
-        _logger.LogInformation("Seeding completed.");
+        
 
 
     }
